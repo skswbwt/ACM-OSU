61d2a653ae8257524fea5256413d9ccf236827bc
Moustafa Eid <eid.6@osu.edu>
2011-01-19 01:27:37 -0500

    finishing prob archive

--- sites/all/modules/problem_archive/problem_archive.js
+++ sites/all/modules/problem_archive/problem_archive.js
@@ -3,5 +3,15 @@
 	$(document).ready( function()
 	{
 		$(".sticky-header").remove();
+		
+		$("th a").add(".pager a").click( function(e)
+		{
+			$("#problem-archive-search-form").attr("action", $(this).attr("href") );
+			
+			$("#problem-archive-search-form").submit();
+			
+			e.preventDefault();
+		});
 	});
+	
 })(jQuery);
